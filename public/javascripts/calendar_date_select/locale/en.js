b507cda9e4481eb9613b6a7e72e0c93ae4253312
kikito <kikito@gmail.com>
2009-07-30 10:39:25 +0200

    Added english locale so making :locale => :en works

---
+++ public/javascripts/calendar_date_select/locale/en.js
@@ -0,0 +1,11 @@
+Date.weekdays = $w("S M T W T F S");
+Date.months = $w("January February March April May Jun July August September October November December" );
+
+Date.first_day_of_week = 7;
+
+_translations = {
+  "OK": "OK",
+  "Now": "Now",
+  "Clear": "Clear",
+  "Today": "Today"
+}
